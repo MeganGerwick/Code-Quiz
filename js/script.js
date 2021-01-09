0f15a8dacb8d50879d3ba871795d3669a0452a06
Louisa May <mgerwick@gmail.com>
2021-01-09 12:36:23 -0600

    Set variables for javascript

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//Set variables
+var score = 0
+var indexQ = 0
+var currentQ = questions[indexQ];
+var welcomeHeading = document.querySelector("#heading");
+var startBtn = document.querySelector("#start-btn");
+var questionTitle = document.querySelector("#question-title");
+
+//Welcome message
+
+//Test questions Array
+var questions = [
+    {
+        question: "",
+        answers: ["", "", ""],
+        correct: ""
+    },
+    {
+        question: "",
+        answers: ["", "", ""],
+        correct: ""
+    },
+    {
+        question: "",
+        answers: ["", "", ""],
+        correct: ""
+    },
+    {
+        question: "",
+        answers: ["", "", ""],
+        correct: ""
+    },
+    {
+        question: "",
+        answers: ["", "", ""],
+        correct: ""
+    },
+    {
+        question: "",
+        answers: ["", "", ""],
+        correct: ""
+    },
+];
+
