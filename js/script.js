c7252f9656d3058a64657a7faf6ca02c8faa88fa
Louisa May <mgerwick@gmail.com>
2021-01-09 12:39:03 -0600

    Create function to render welcome message

--- js/script.js
+++ js/script.js
@@ -7,7 +7,11 @@ var startBtn = document.querySelector("#start-btn");
 var questionTitle = document.querySelector("#question-title");
 
 //Welcome message
-
+function welcomeMessage() {
+    welcomeHeading.textContent = "Coding Quiz Challenge";
+    questionTitle.textContent = "It's time to test your knowledge of JavaScript! You will have 60 seconds to answer 6 questions. Click the start button to begin.";
+};
+welcomeMessage();
 //Test questions Array
 var questions = [
     {
